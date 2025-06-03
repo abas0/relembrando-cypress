4fe017f6e66812efd65d934e6f30384d87581136
Beatriz Sousa <beatriz.a.sousa@avanade.com>
2025-06-03 13:28:37 -0300

    aaaa

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,14 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+const path = require('path');
+
+// Lê o valor de process.env.ENVIRONMENT passado via GitHub Actions ou localmente
+const environment = process.env.ENVIRONMENT || 'dev'; // fallback para dev
+
+// Carrega o config correto dinamicamente
+const envConfig = require(`./config-${environment}.js`);
 
 module.exports = defineConfig({
-  reporter: 'cypress-multi-reporters',
-  reporterOptions: {
-    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
-    mochaJunitReporterReporterOptions: {
-      mochaFile: 'cypress/reports/junit/results-[hash].xml'
-    },
-    cypressMochawesomeReporterReporterOptions: {
-      charts: true,
-      reportPageTitle: 'Relatório de testes',
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveAllAttempts: false
-    }
-  },
-  chromeWebSecurity: false,
   e2e: {
-    setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on)
-    },
-  },
+    ...envConfig
+  }
 });
