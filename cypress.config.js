5b7772fe529f6fd5493375365fca59dd16910d23
Beatriz Sousa <beatriz.a.sousa@avanade.com>
2025-06-04 11:51:14 -0300

    publicando relatorios como artefato

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,27 @@ const environment = process.env.ENVIRONMENT || 'dev'; // fallback para dev
 const envConfig = require(`./config-${environment}.js`);
 
 module.exports = defineConfig({
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+    mochaJunitReporterReporterOptions: {
+      mochaFile: 'cypress/reports/junit/results-[hash].xml'
+    },
+    cypressMochawesomeReporterReporterOptions: {
+      charts: true,
+      reportPageTitle: 'Relatório de testes',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false
+    }
+  },
+  chromeWebSecurity: false,
+  video: true,
   e2e: {
-    ...envConfig
+    ...envConfig,
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      // você pode adicionar outros plugins ou listeners aqui
+    }
   }
 });
